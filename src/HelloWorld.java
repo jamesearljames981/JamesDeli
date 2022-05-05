9ff004627491c95f9a81be7e092b900b9452f4ee
jamesearljames981 <104846322+jamesearljames981@users.noreply.github.com>
2022-05-05 16:13:27 -0400

    Java Practice 'not practing'

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+ public class HelloWorld {
+        public static void main(String[] args) {
+            String s = ("Hello World ");
+            String s1 = new String ("I'm James");
+            System.out.println(s.toLowerCase()+s1.toUpperCase());
+        }
+    }
+
